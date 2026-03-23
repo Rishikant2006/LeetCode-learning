356deb36530afb514655d755e978e8468a1aa544
Rishikant2006 <155076480+Rishikant2006@users.noreply.github.com>
2026-03-23 09:49:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (96.89%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        // Step 1: Push element into q2
+        q2.add(x);
+
+        // Step 2: Move all elements from q1 to q2
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+
+        // Step 3: Swap q1 and q2
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+    
+    public int pop() {
+        return q1.remove();   // removes top element
+    }
+    
+    public int top() {
+        return q1.peek();     // returns top element
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
